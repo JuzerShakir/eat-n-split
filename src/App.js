6c8b4948a89aefe761112f2908907e22095164ae
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-07-05 13:46:32 +0530

    list all the friends

--- src/App.js
+++ src/App.js
@@ -22,5 +22,48 @@ const initialFriends = [
 ];
 
 export default function App() {
-  console.log(initialFriends);
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
+  );
+}
+
+function FriendList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ₹{Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ₹{Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
 }
